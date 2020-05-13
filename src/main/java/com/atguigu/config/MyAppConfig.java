e85c5f656559fda7a623110e0f5f141ee482e7b2
shawnGlobal <13600410992Global@163.com>
2020-05-13 08:06:54 +0800

    888

--- src/main/java/com/atguigu/config/MyAppConfig.java
+++ src/main/java/com/atguigu/config/MyAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ImportResource;
  * @Configuration 指明当前类是一个配置类（其实用@Component 是一样的），就是用来代替之前的Spring配置文件的
  * 
  * 在配置文件中是用bean标签添加组件的
- * 测试777.
+ * 测试888.
  *
  */
 //@ImportResource(locations = {"classpath:beans.xml"})
@@ -21,8 +21,8 @@ public class MyAppConfig {
 	//将方法的返回值添加到容器中；容器中这个组件默认的id就是方法名
 	@Bean
 	public HelloService helloService2() {
-		System.out.println("@Bean 给容器添加组件：helloService2");
-		return new HelloService();
-	}
+        System.out.println("@Bean 给容器添加组件：helloService2");
+        return new HelloService();
+    }
 	
 }
